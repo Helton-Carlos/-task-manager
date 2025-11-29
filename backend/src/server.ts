8578f7e849352b2b6d11ff30d01e37817d7fb763
Helton Brito <heltoncarlospa@hotmail.com>
2025-11-29 14:41:30 -0300

    fix: config env

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,10 @@
 import express from 'express';
 import cors from 'cors';
-import 'dotenv/config';
+import dotenv from 'dotenv';
 
 import userRoutes from './routes/user';
 
+dotenv.config();
 const app = express();
 
 app.use(cors());
